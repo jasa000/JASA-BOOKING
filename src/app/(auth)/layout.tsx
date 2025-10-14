5119ae83f19833d14108f05aba04dce4fccabf32
JASA Essential <essentialjasa@gmail.com>
2025-10-14 09:11:28 +0000

    Make the header to be seen in Login and registration page

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,16 @@
+import { SiteHeader } from "@/components/site-header";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen items-center justify-center p-4">
-      {children}
+    <div className="relative flex min-h-screen flex-col">
+      <SiteHeader />
+      <main className="flex-1 flex items-center justify-center p-4">
+        {children}
+      </main>
     </div>
   );
 }
