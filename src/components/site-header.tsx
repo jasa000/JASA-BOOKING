60322605094c10496390a19eab57d8fe20e9c018
JASA Essential <essentialjasa@gmail.com>
2025-10-17 05:46:28 +0000

    The options in header is unable to scroll

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -6,34 +6,39 @@ import { MainNav } from "@/components/main-nav"
 import { UserNav } from "@/components/user-nav"
 import { Button } from "@/components/ui/button"
 import { ThemeToggle } from "./theme-toggle"
-import { ScrollArea } from "./ui/scroll-area"
+import { ScrollArea, ScrollBar } from "./ui/scroll-area"
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <ScrollArea className="w-full whitespace-nowrap">
-        <div className="container flex h-16 min-w-[370px] items-center justify-between gap-4 px-4 md:px-6">
-          <div className="flex items-center gap-2">
-            <Link href="/">
-              <Button variant="ghost" size="icon">
-                <Home className="h-5 w-5" />
-                <span className="sr-only">Home</span>
-              </Button>
-            </Link>
-            <ThemeToggle />
-          </div>
-          <MainNav />
-          <div className="flex items-center justify-end gap-2">
-              <Link href="/events/create">
-                  <Button variant="ghost" size="icon">
-                      <PlusSquare className="h-5 w-5" />
-                      <span className="sr-only">Create Event</span>
-                  </Button>
+      <div className="container px-4 md:px-6">
+        <ScrollArea className="w-full whitespace-nowrap">
+          <div className="flex h-16 items-center justify-between gap-4">
+            <div className="flex items-center gap-2">
+              <Link href="/">
+                <Button variant="ghost" size="icon">
+                  <Home className="h-5 w-5" />
+                  <span className="sr-only">Home</span>
+                </Button>
               </Link>
-              <UserNav />
+              <ThemeToggle />
+            </div>
+            <div className="flex-1 flex justify-center">
+              <MainNav />
+            </div>
+            <div className="flex items-center justify-end gap-2">
+                <Link href="/events/create">
+                    <Button variant="ghost" size="icon">
+                        <PlusSquare className="h-5 w-5" />
+                        <span className="sr-only">Create Event</span>
+                    </Button>
+                </Link>
+                <UserNav />
+            </div>
           </div>
-        </div>
-      </ScrollArea>
+          <ScrollBar orientation="horizontal" className="invisible" />
+        </ScrollArea>
+      </div>
     </header>
   )
 }
