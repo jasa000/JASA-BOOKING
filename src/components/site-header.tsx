dad6b69ddcbc5a60ce6b8d70a439c8faa707b9e5
JASA Essential <essentialjasa@gmail.com>
2025-10-18 12:13:33 +0000

    The post button is not available

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -19,17 +19,13 @@ export function SiteHeader() {
             : "dark:bg-black"
     )}>
       <div className="relative flex h-16 items-center px-4 md:px-6">
-        <div className="flex items-center">
-            {/* Left side content can go here if needed in the future */}
-        </div>
-        
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
             <MainNav />
         </div>
 
         <div className="flex flex-1 items-center justify-end space-x-4">
           <Link href="/events/create">
-              <Button variant="ghost" className="hidden md:flex">
+              <Button variant="ghost">
                   <Plus className="mr-2 h-4 w-4" />
                   Post
               </Button>
