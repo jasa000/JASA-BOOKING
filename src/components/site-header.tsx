3ad7f69ff18bd9c63978978c603e1eb1aec0382f
JASA Essential <essentialjasa@gmail.com>
2025-10-18 12:09:03 +0000

    Ok

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -4,6 +4,7 @@
 import { MainNav } from "./main-nav"
 import { useTheme } from "./theme-provider"
 import { cn } from "@/lib/utils"
+import { UserNav } from "./user-nav"
 
 export function SiteHeader() {
   const { theme } = useTheme();
@@ -14,8 +15,18 @@ export function SiteHeader() {
             ? "bg-primary text-primary-foreground"
             : "dark:bg-black"
     )}>
-      <div className="relative flex h-16 items-center justify-center px-4 md:px-6">
-        <MainNav />
+      <div className="relative flex h-16 items-center justify-between px-4 md:px-6">
+        <div className="flex items-center">
+            {/* Left side content can go here if needed in the future */}
+        </div>
+        
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <MainNav />
+        </div>
+
+        <div className="flex items-center justify-end">
+          <UserNav />
+        </div>
       </div>
     </header>
   )
