9533bb59e5aa3780ad460a40e344731b406cacef
JASA Essential <essentialjasa@gmail.com>
2025-10-18 12:18:59 +0000

    Add a theme switch button icon near home icon

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -7,6 +7,7 @@ import { UserNav } from "./user-nav"
 import { Button } from "./ui/button"
 import Link from "next/link"
 import { Plus, Home } from "lucide-react"
+import { ThemeToggle } from "./theme-toggle"
 
 export function SiteHeader() {
   return (
@@ -14,13 +15,14 @@ export function SiteHeader() {
         "sticky top-0 z-40 w-full border-b bg-background"
     )}>
       <div className="relative flex h-16 items-center px-4 md:px-6">
-        <div className="flex flex-1 items-center">
+        <div className="flex flex-1 items-center space-x-2">
            <Link href="/" passHref>
             <Button variant="ghost" size="icon">
               <Home className="h-5 w-5" />
               <span className="sr-only">Home</span>
             </Button>
           </Link>
+          <ThemeToggle />
         </div>
 
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
