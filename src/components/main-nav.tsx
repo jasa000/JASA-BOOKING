04b08b41442d4d46139bb8ba27e010b23b77cd47
JASA Essential <essentialjasa@gmail.com>
2025-10-18 12:04:34 +0000

    The booking text to be clear allined to where B to j and G to A

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -17,13 +17,13 @@ export function MainNav({
       {...props}
     >
       <Link href="/" className="flex flex-col items-center space-y-0">
-        <span 
-          className="font-bold font-headline text-lg leading-none"
-          style={{ letterSpacing: '0.4em' }}
-        >
-          JASA
-        </span>
-        <span className="font-headline text-sm leading-none pl-1">
+        <div className="flex w-full justify-between">
+            <span className="font-bold font-headline text-lg leading-none">J</span>
+            <span className="font-bold font-headline text-lg leading-none">A</span>
+            <span className="font-bold font-headline text-lg leading-none">S</span>
+            <span className="font-bold font-headline text-lg leading-none">A</span>
+        </div>
+        <span className="font-headline text-sm leading-none">
           BOOKING
         </span>
       </Link>
