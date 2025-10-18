944eff294dae70cf57bd32cd6aecc2105806e54f
JASA Essential <essentialjasa@gmail.com>
2025-10-18 12:18:02 +0000

    Add a home icon in left side of header

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -2,21 +2,27 @@
 "use client"
 
 import { MainNav } from "./main-nav"
-import { useTheme } from "./theme-provider"
 import { cn } from "@/lib/utils"
 import { UserNav } from "./user-nav"
 import { Button } from "./ui/button"
 import Link from "next/link"
-import { Plus } from "lucide-react"
+import { Plus, Home } from "lucide-react"
 
 export function SiteHeader() {
-  const { theme } = useTheme();
   return (
     <header className={cn(
-        "sticky top-0 z-40 w-full border-b",
-        "bg-background"
+        "sticky top-0 z-40 w-full border-b bg-background"
     )}>
       <div className="relative flex h-16 items-center px-4 md:px-6">
+        <div className="flex flex-1 items-center">
+           <Link href="/" passHref>
+            <Button variant="ghost" size="icon">
+              <Home className="h-5 w-5" />
+              <span className="sr-only">Home</span>
+            </Button>
+          </Link>
+        </div>
+
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
             <MainNav />
         </div>
